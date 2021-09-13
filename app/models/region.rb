b8160af9ecd6331c306e55d339ac9cc4aac2be88
Saalik Lokhandwala <saalikl111@gmail.com>
2021-09-13 07:50:58 -0500

    edit region model test, fix typo in migration

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,7 +2,6 @@ class Region < ApplicationRecord
   validates :city, presence: true
   validates :state, presence: true
   validates :country, presence: true
-  validates :city, presence: true
   
   has_many :restaurants
 end
--- db/migrate/20210729155912_create_region.rb
+++ db/migrate/20210729155912_create_region.rb
@@ -3,7 +3,7 @@ class CreateRegion < ActiveRecord::Migration[6.1]
     create_table :regions do |t|
       t.string :city, null: false
       t.string :state, null: false
-      t.string :coutnry, null: false
+      t.string :country, null: false
       t.string :img_url
       
       t.timestamps
