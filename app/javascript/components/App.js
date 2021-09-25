ee852eec5267581edaa8023d1f1ad53029aed68e
Saalik Lokhandwala <saalikl111@gmail.com>
2021-09-25 06:24:46 -0500

    cleanup react files, remove react-router-dom

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,8 @@
 import React from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <div>Hello World Index</div>
-        </Route>
-        <Route exact path="/regions/:id">
-          <div>REGION PAGE</div>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div>OH HEY ITS REACT</div>
   )
 }
 
