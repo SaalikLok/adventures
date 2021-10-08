017be81da56184027b55b3b9a1f1e0cdc0018cb0
Saalik Lokhandwala <saalikl111@gmail.com>
2021-10-07 21:38:11 -0500

    add restaurants form, feature tests, controller, routes, flash messages

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,23 @@
+class RestaurantsController < ApplicationController
+  def new
+    @region_cities = Region.select(:city).map { |region| region.city }
+  end
+
+  def create
+    @restaurant = Restaurant.new(
+      region: Region.find_by(city: params[:region]),
+      name: params[:name],
+      description: params[:description],
+      rating: params[:rating],
+      cuisine: params[:cuisine],
+      notes: params[:notes]
+    )
+    
+    if @restaurant.save
+      redirect_to region_path(@restaurant.region)
+    else
+      flash[:alert] = "Whoops! #{@restaurant.errors.full_messages.to_sentence}"
+      redirect_to new_restaurant_path
+    end
+  end
+end
