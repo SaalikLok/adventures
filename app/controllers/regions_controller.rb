c4ddaf12dfc44adc17835f7860249c931785039d
Saalik Lokhandwala <saalikl111@gmail.com>
2021-11-13 13:24:02 -0500

    move fun ratings logic to react

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -5,17 +5,6 @@ class RegionsController < ApplicationController
 
   def show
     @region = Region.find(params[:id])
-    raw_restaurants = @region.restaurants.order(rating: :desc)
-    @restaurants = transform_rating(raw_restaurants)
-  end
-
-  protected
-
-  def transform_rating(restaurants)
-    str_ratings = ["avoid", "meh", "good", "great", "AMAZE"]
-
-    restaurants.each do |restaurant|
-      restaurant.rating = str_ratings[restaurant.rating.to_i - 1]
-    end
+    @restaurants = @region.restaurants.order(rating: :desc)
   end
 end
